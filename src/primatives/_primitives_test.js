3b115cb1c087a0e7e03385df3119f893b9e183a5
HySteffensen <hyrum.steffensen@gmail.com>
2018-06-03 19:42:12 -0700

    null values

--- src/primatives/_primitives_test.js
+++ src/primatives/_primitives_test.js
@@ -1,10 +1,69 @@
-"use strict";
+/* jshint -W117, -W116, strict:false */
 
 var assert = require('chai').assert;
-var primatives = require('./primitives');
+var primitives = require('./primitives');
 
-describe("Primatives", function() {
-    it("type undefined", function() {
-        assert(primatives.returnsUndefined(), undefined);
+describe("Undeclared and Undefined Variables", function() {
+    describe("Undeclared Primitives", function() {
+        it("returns String 'undefined' when not declared", function() {
+            assert.equal(primitives.returnsStringUndefinedWhenNotDeclared(), 'undefined');
+        });
+        
+        it("variable throws error when not declared", function() {
+            assert.throws(function() {
+                primitives.returnsUndefinedWhenNotDeclared();
+            });
+        });
+    });
+
+    describe("Undefined Primitives", function() {
+        it("returns undefined when declared but not defined", function() {
+            assert.equal(primitives.returnsUndefinedWhenDeclaredButNotDefined(), undefined);
+        });
+
+        it("returns String 'undefined' when declared but not defined using typeof", function() {
+            assert.equal(primitives.returnsStringUndefinedWhenDeclaredButNotDefinedUsingTypeof(), 'undefined');
+        });
+
     });
 });
+
+describe("null", function() {
+    it("typeof(null) equals 'object' ", function() {
+        // null is typeof "object" because of legacy reasons
+        assert.notEqual(typeof(null), null);
+        assert.equal(typeof(null), "object");
+        assert.isTrue(typeof(null) == "object");
+        assert.isTrue(typeof(null) === "object");
+    });
+
+    it("coerces to equal undefined", function() {
+        assert.isTrue(null == undefined);
+        assert.isFalse(null === undefined);
+    });
+
+    it("null equals null", function() {
+        assert.isTrue(null == null);
+        assert.isTrue(null === null);
+    });
+
+    it("!null", function() {
+        assert.isTrue(!null);
+        // assert.isFalse(null);
+    });
+
+    it("null + 1 is a number", function() {
+        assert.isFalse(isNaN(null + 1));
+    });
+
+    it("null + 1 is 1", function() {
+        assert.equal(null + 1, 1);
+    });
+
+    it("null + undefined is not a number", function() {
+        assert.isTrue(isNaN(null + undefined));
+    });
+});
+
+
+
--- src/primatives/primitives.js
+++ src/primatives/primitives.js
@@ -1,2 +1,21 @@
-"use strict";
+/* jshint -W117, strict:false */
+
+
+exports.returnsStringUndefinedWhenNotDeclared = function() {
+    return typeof(test);
+};
+
+exports.returnsUndefinedWhenNotDeclared = function() {
+    if (t === undefined) throw new Error("Variable Undeclared");
+};
+
+exports.returnsUndefinedWhenDeclaredButNotDefined = function() {
+    var test;
+    return test;
+};
+
+exports.returnsStringUndefinedWhenDeclaredButNotDefinedUsingTypeof = function() {
+    var test;
+    return typeof(test);
+};
 
